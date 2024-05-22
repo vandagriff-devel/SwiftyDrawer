14c716337cb619e9b2eea818547396ae26a85ad2
vandagriffk <vandagriff.devel@gmail.com>
2024-05-21 15:38:11 -1000

    Initial Commit

---
+++ SwiftyDrawer/SwiftyDrawer.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyDrawer.h
+//  SwiftyDrawer
+//
+//  Created by Keith Vandagriff on 5/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyDrawer.
+FOUNDATION_EXPORT double SwiftyDrawerVersionNumber;
+
+//! Project version string for SwiftyDrawer.
+FOUNDATION_EXPORT const unsigned char SwiftyDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDrawer/PublicHeader.h>
+
+
